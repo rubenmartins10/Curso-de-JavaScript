0ed2c4c5fa4abc5f6440995c1f8758c3cb731554
rubenmartins10 <rubendavidsilvamartins@gmail.com>
2026-02-04 00:56:37 +0000

    Variáveis, numerais e literais

---
+++ Aulas/Variáveis, numerais, literais.md
@@ -0,0 +1,8 @@
+- Os dados numerais são interpretados por JavaScript como valores numéricos, e vão sem aspas.
+  - Os valores literais vão entre aspas.
+
+- Para não extrairmos um número ele deve ser considerado parte da frase e da composição literal.
+
+- As variáveis permitem manipular dados na forma dinâmica.
+
+- Para definição das variáveis não vamos usar espaços nem utilizaremos caracteres especiais.
---
+++ JavaScript_Programação/3-1.js
@@ -0,0 +1,15 @@
+
+var mensagem = "Hello, World!";
+
+//perfeito para manipular dados de maneira efetiva e eficiente
+
+var nome = "David";
+var idade = 22; //futuramente de as definirmos podemos utiliza-las em diversas entradas, garantindo flexibilidade 
+
+console.log(mensagem); //supostamente tem de exibir "Hello, World!" no console 
+
+var mensagem = "Olá, Mundo!"; //redefinindo a variável mensagem para o português
+                              //podemos refefinir as variáveis conforme necessário
+
+console.log(nome);
+console.log(idade);
